3ef7448ef46a73c595f6b64648be93a9c1ea1bae
Christian Ortiz <christianortiz319@yahoo.com>
2015-10-15 02:08:27 -0700

    Create TotalPurchases.h

---
+++ Warehouse/windows/TotalPurchases.h
@@ -0,0 +1,29 @@
+#ifndef WINDOWS_TOTALPURCHASES_H_
+#define WINDOWS_TOTALPURCHASES_H_
+
+
+#include "../core/Initializer.h"
+
+#include "Window.h"
+
+class TotalPurchases : public Window {
+
+private:
+	int state;
+	int fail;
+public:
+	TotalPurchases(int *p_a_d, Item ** i, int *n_i, Member **m,
+			int *n_m, Trip **t, int n_d) : Window(p_a_d, i, n_i, m, n_m, t, n_d) {
+		ID = TOTALPURCHASES;
+		state = 0;
+		fail = 0;
+
+
+	}
+	~TotalPurchases(){}
+	void render_main(zr_window *window);
+};
+
+
+
+#endif /* WINDOWS_TOTALPURCHASES_H_ */
